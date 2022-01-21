e7172a46673f06fd7499cda58a8254df10e1223e
XRimuruTempestX <894273414@qq.com>
2022-01-21 22:35:10 +0800

    练习

---
+++ 输入输出练习/输入输出练习/练习.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//¥Ú”°hello world
+	cout << "hello world" << endl;
+
+	//º∆À„≥À∑®
+	int v2 = 1, v3 = 2;
+	cout << v2 * v3 << endl;
+
+	
+	return 0;
+}
